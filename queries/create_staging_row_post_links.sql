c624fd1e2c391b2a7b0c3240fb66d5aa26bdcd25
bellamartirosyan <57104922+bellamartirosyan@users.noreply.github.com>
2023-05-01 11:01:25 +0400

    Add files via upload

---
+++ queries/create_staging_row_post_links.sql
@@ -0,0 +1,8 @@
+CREATE TABLE velvety-ring-349218.Stack_Overflow.staging_row_post_links (
+    id INT,
+    related_post_id INT,
+    link_type_id INT,
+    creation_date TIMESTAMP,
+    uuid STRING,
+    ingestion_date date 
+);
