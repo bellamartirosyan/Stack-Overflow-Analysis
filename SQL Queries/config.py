969b043fa3b28dd44c41376fc86025a810d6aaa8
bellamartirosyan <57104922+bellamartirosyan@users.noreply.github.com>
2023-05-01 11:06:59 +0400

    Delete config.py

--- SQL Queries/config.py
+++
@@ -1,8 +0,0 @@
-cred_json = 'velvety-ring-349218-f9ffa94dc6a5.json'
-project_id = 'velvety-ring-349218'
-dataset_id = 'Stack_Overflow'
-#table_name = 'staging_data'
-table_name = 'staging_data1'
-dataset_id = 'Stack_Overflow'
-folder_id = '1KDmKD807Tlxdr2m-Kls4UFoO0oicO0th'
-queries = '/Users/izabellamartirosyan/Desktop/Capstone/queries'
