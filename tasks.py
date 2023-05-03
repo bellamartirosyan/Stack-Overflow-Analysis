9cf55fdcf9bc89ddedfdba82b8468ddb7a00fd9d
bellamartirosyan <57104922+bellamartirosyan@users.noreply.github.com>
2023-05-03 21:16:24 +0400

    tasks.py

--- tasks.py
+++ tasks.py
@@ -9,6 +9,14 @@ import pandas as pd
 import pandas_gbq
 import uuid
 import argparse
+from logging import logger
+def my_function():
+    logging.basicConfig(format='%(asctime)s - %(message)s', level=logging.INFO)
+    logger.debug('This is a debug message')
+    logger.info('This is an info message')
+    logger.warning('This is a warning message')
+    logger.error('This is an error message')
+    logger.critical('This is a critical message') 
 
 # Authenticate and create a BigQuery client
 def create_client(cred_json, project_id):
