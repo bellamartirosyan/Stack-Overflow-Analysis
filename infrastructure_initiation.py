2da206f47fafe9613f21212ae676950065280b37
bellamartirosyan <57104922+bellamartirosyan@users.noreply.github.com>
2023-05-04 22:59:33 +0400

    Update infrastructure_initiation.py

--- infrastructure_initiation.py
+++ infrastructure_initiation.py
@@ -1,45 +1,150 @@
 import config
 import tasks
 
+if not os.path.exists(log_folder):
+    os.makedirs(log_folder)
+
+
+
 
 if __name__ == '__main__':
     client = tasks.create_client(cred_json=config.cred_json, project_id=config.project_id)
 
-    # dropping tables if they exist
-    tasks.drop_table(client=client, project_id=config.project_id, dataset_id=config.dataset_id,
-                     table_name='users_dim')
-    tasks.drop_table(client=client, project_id=config.project_id, dataset_id=config.dataset_id,
-                     table_name='tags_dim')
-    tasks.drop_table(client=client, project_id=config.project_id, dataset_id=config.dataset_id,
-                     table_name='posts_question_dim')
-    tasks.drop_table(client=client, project_id=config.project_id, dataset_id=config.dataset_id,
-                     table_name='badges_fact')
-    tasks.drop_table(client=client, project_id=config.project_id, dataset_id=config.dataset_id,
-                     table_name='comments_fact')
-    tasks.drop_table(client=client, project_id=config.project_id, dataset_id=config.dataset_id,
-                     table_name='post_answers_fact')
-    tasks.drop_table(client=client, project_id=config.project_id, dataset_id=config.dataset_id,
-                     table_name='post_history_fact')
-    tasks.drop_table(client=client, project_id=config.project_id, dataset_id=config.dataset_id,
-                     table_name='post_links_fact')
+    logging.info(f"Client object has been created in {config.project_id}")
+
+    # creating the schema
+    tasks.create_schema(
+        client=client, project_id=config.project_id, dataset_id=config.dataset_id
+    )
+    logging.info(f"Schema {config.dataset_id }has been created in {config.project_id}")
+
+     # dropping tables if they exist
+    tasks.drop_table(
+        client=client,
+        project_id=config.project_id,
+        dataset_id=config.dataset_id,
+        table_name="badges_fact",
+    )
+    logging.info("badges_fact has been dropped")
+    tasks.drop_table(
+        client=client,
+        project_id=config.project_id,
+        dataset_id=config.dataset_id,
+        table_name="comments_fact",
+    )
+    logging.info("comments_fact has been dropped")
+    tasks.drop_table(
+        client=client,
+        project_id=config.project_id,
+        dataset_id=config.dataset_id,
+        table_name="",
+    )
+    logging.info("post_answers_fact has been dropped")
+    tasks.drop_table(
+        client=client,
+        project_id=config.project_id,
+        dataset_id=config.dataset_id,
+        table_name="post_answers_fact",
+    )
+    logging.info("post_answers_fact has been dropped")
+    tasks.drop_table(
+        client=client,
+        project_id=config.project_id,
+        dataset_id=config.dataset_id,
+        table_name="post_history_fact",
+    )
+    logging.info("post_history_fact has been dropped")
+    tasks.drop_table(
+        client=client,
+        project_id=config.project_id,
+        dataset_id=config.dataset_id,
+        table_name="post_links_fact",
+    )
+    logging.info("post_links_fact has been dropped")
+    tasks.drop_table(
+        client=client,
+        project_id=config.project_id,
+        dataset_id=config.dataset_id,
+        table_name="users_dim",
+    )
+    logging.info("users_dim has been dropped")
+    tasks.drop_table(
+        client=client,
+        project_id=config.project_id,
+        dataset_id=config.dataset_id,
+        table_name="tags_dim",
+    )
+    logging.info("tags_dim has been dropped")
+    tasks.drop_table(
+        client=client,
+        project_id=config.project_id,
+        dataset_id=config.dataset_id,
+        table_name="posts_question_dim",
+    )
+    logging.info("posts_question_dim has been dropped")
 
     # creating tables
-    tasks.create_table(client=client, project_id=config.project_id, dataset_id=config.dataset_id,
-                       table_name='users_dim')
-    tasks.create_table(client=client, project_id=config.project_id, dataset_id=config.dataset_id,
-                       table_name='tags_dim')
-    tasks.create_table(client=client, project_id=config.project_id, dataset_id=config.dataset_id,
-                       table_name='posts_question_dim')
-    tasks.create_table(client=client, project_id=config.project_id, dataset_id=config.dataset_id,
-                       table_name='badges_fact')
-    tasks.create_table(client=client, project_id=config.project_id, dataset_id=config.dataset_id,
-                       table_name='comments_fact')
-    tasks.create_table(client=client, project_id=config.project_id, dataset_id=config.dataset_id,
-                       table_name='post_answers_fact')
-    tasks.create_table(client=client, project_id=config.project_id, dataset_id=config.dataset_id,
-                       table_name='post_history_fact')
-    tasks.create_table(client=client, project_id=config.project_id, dataset_id=config.dataset_id,
-                       table_name='post_links_fact')
-
-
+    tasks.create_table(
+        client=client,
+        project_id=config.project_id,
+        dataset_id=config.dataset_id,
+        table_name="badges_fact",
+    )
+    logging.info("badges_fact has been created")
+    tasks.create_table(
+        client=client,
+        project_id=config.project_id,
+        dataset_id=config.dataset_id,
+        table_name="comments_fact",
+    )
+    logging.info("comments_fact has been created")
+    tasks.create_table(
+        client=client,
+        project_id=config.project_id,
+        dataset_id=config.dataset_id,
+        table_name="comments_fact",
+    )
+    logging.info("DimPlatform_SCD1 has been created")
+    tasks.create_table(
+        client=client,
+        project_id=config.project_id,
+        dataset_id=config.dataset_id,
+        table_name="post_answers_fact",
+    )
+    logging.info("post_answers_fact has been created")
+    tasks.create_table(
+        client=client,
+        project_id=config.project_id,
+        dataset_id=config.dataset_id,
+        table_name="post_links_fact",
+    )
+    logging.info("post_links_fact has been created")
+    tasks.create_table(
+        client=client,
+        project_id=config.project_id,
+        dataset_id=config.dataset_id,
+        table_name="users_dim",
+    )
+    logging.info("users_dim has been created")
+    tasks.create_table(
+        client=client,
+        project_id=config.project_id,
+        dataset_id=config.dataset_id,
+        table_name="tags_dim",
+    )
+    logging.info("tags_dim has been created")
+    tasks.create_table(
+        client=client,
+        project_id=config.project_id,
+        dataset_id=config.dataset_id,
+        table_name="posts_question_dim",
+    )
+    logging.info("posts_question_dim has been created")
+    tasks.create_table(
+        client=client,
+        project_id=config.project_id,
+        dataset_id=config.dataset_id,
+        table_name="FactHouse_SCD2",
+    )
+    logging.info("posts_question_dim has been created")
 
