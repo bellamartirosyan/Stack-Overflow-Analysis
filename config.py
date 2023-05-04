8615bfb66b1e04e31a70695502411ae19ee61172
bellamartirosyan <57104922+bellamartirosyan@users.noreply.github.com>
2023-05-04 22:42:17 +0400

    config.py

---
+++ config.py
@@ -0,0 +1,13 @@
+cred_json = "velvety-ring-349218-f9ffa94dc6a5.json"
+project_id = "velvety-ring-349218"
+# table_name = 'staging_data'
+table_name = "staging_data1"
+dataset_id = "Stack_Overflow"
+folder_id = "1KDmKD807Tlxdr2m-Kls4UFoO0oicO0th"
+queries = "/Users/izabellamartirosyan/Desktop/Capstone/queries"
+client_config_file = "/Users/izabellamartirosyan/Desktop/Capstone/client_secrets.json"
+log_folder = "/Users/izabellamartirosyan/Desktop/Capstone/logs"
+log_file_flow = "flow.log"
+log_file_infr = "infrastructure_initiation.log"
+log_file_scrap = "scraping.log"
+log_file_prep = "preprocessing.log"
