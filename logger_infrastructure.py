37ed74f62b6d6ddb77ac7c621060494bf31b2b57
bellamartirosyan <57104922+bellamartirosyan@users.noreply.github.com>
2023-05-11 22:39:04 +0400

    Update logger_infrastructure.py

--- logger_infrastructure.py
+++ logger_infrastructure.py
@@ -2,6 +2,9 @@ import logging
 from config import log_folder, log_file_infr
 import os
 
+logfolder = os.getcwd() + '\\' + log_folder
+if not os.path.exists(logfolder):
+    os.makedirs(logfolder)
 
 logging.basicConfig(
     level=logging.INFO,
