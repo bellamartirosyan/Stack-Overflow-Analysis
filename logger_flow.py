dde9cbea1cd97291a23c3c439dd689cfef3f1b66
bellamartirosyan <57104922+bellamartirosyan@users.noreply.github.com>
2023-05-11 23:25:13 +0400

    Update logger_flow.py

--- logger_flow.py
+++ logger_flow.py
@@ -1,10 +1,12 @@
 import logging
-from config import log_folder, log_file_flow
+from config import log_folder, log_file_infr
 import os
 
+logfolder = os.getcwd() + '\\' + log_folder
+if not os.path.exists(logfolder):
+    os.makedirs(logfolder)
 
 logging.basicConfig(
     level=logging.INFO,
-    filename=os.path.join(log_folder, log_file_flow),
+    filename=os.path.join(log_folder, log_file_infr),
     format="%(asctime)s - %(levelname)s - %(message)s",
-)
