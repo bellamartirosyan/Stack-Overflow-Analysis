1920a12b4a2f839a7809bf59d6ed05f4f50c10a9
bellamartirosyan <57104922+bellamartirosyan@users.noreply.github.com>
2023-05-15 19:31:17 +0400

    Update tasks.py

--- tasks.py
+++ tasks.py
@@ -9,6 +9,13 @@ import pandas as pd
 import pandas_gbq
 import uuid
 import argparse
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+from google.oauth2.credentials import Credentials
+from google.oauth2 import service_account
+from google_auth_oauthlib.flow import InstalledAppFlow
+import time 
+
 
 # def my_function():
 #     logger.basicConfig(format='%(asctime)s - %(message)s', level=logger.INFO)
@@ -24,10 +31,39 @@ def create_client(cred_json, project_id):
     client = bigquery.Client(credentials=credentials, project=project_id)
     return client
 
+# Upload data to Google Drive
+def upload_file_from_local_to_drive(gauth_cred, client_config_file,folder_id, filename, file_name):
+    gauth = GoogleAuth()
 
-# def upload_from_local_to_drive(gauth_cred, client_config_file, folder_id, filename, file_name):
-
-#     gauth = GoogleAuth()
+    # Loads previously saved credentials file if available, otherwise authorizes user and saves new credentials.
+    gauth.LoadCredentialsFile(gauth_cred)
+    if gauth.credentials is None:
+        gauth.DEFAULT_SETTINGS['client_config_file'] = client_config_file
+        gauth.LocalWebserverAuth()
+        gauth.SaveCredentialsFile(gauth_cred)
+    drive = GoogleDrive(gauth)
+    file=None
+    # Uploads the new JSON file to a specified Google Drive folder.
+    file = drive.CreateFile({'title': file_name, 'parents': [{'id': folder_id}]})
+    file.SetContentFile(filename)
+    file.Upload()
+  
+   
+    
+def upload_files_from_local_to_drive(gauth_cred,
+                                     client_config_file,
+                                     folder_id,
+                                     download_folder,
+                                     dest_file_template,
+                                     ingestion_date):
+    
+    for filename_template in dest_file_template.values():
+        print(filename_template)
+        destination_file_name = filename_template.format(ingestion_date=ingestion_date.replace("-", ""))
+        downloadfolder = os.getcwd() + '/' + download_folder
+        dest_file_path = os.path.join(downloadfolder, destination_file_name)
+        upload_file_from_local_to_drive(gauth_cred, client_config_file,folder_id=folder_id, filename=dest_file_path, file_name=destination_file_name)
+     
 #Download data from Bigquery
 def retrieve_table(client,project_id,table_name, dataset_id, dest_file_template,ingestion_date,download_folder):
 
@@ -35,7 +71,7 @@ def retrieve_table(client,project_id,table_name, dataset_id, dest_file_template,
     table_id = "{project_id}.{dataset_id}.{table_name}".format(project_id=project_id, dataset_id=dataset_id, table_name=table_name)
 
     # Set the path to the JSON file to save
-    destination_file_name = dest_file_template.format(ingestion_date=ingestion_date.replace("-", " "))
+    destination_file_name = dest_file_template.format(ingestion_date=ingestion_date.replace("-", ""))
     dest_file_path = os.path.join(download_folder, destination_file_name)
     #"/Users/izabellamartirosyan/Desktop/Capstone/post_answers.json"
     retrieve_data_script = load_query("retrieve_data_{}".format(table_name)).format(
@@ -50,7 +86,6 @@ def retrieve_table(client,project_id,table_name, dataset_id, dest_file_template,
 #retrive data
 def retrieve_data(client,project_id, table_dict, dataset_id,ingestion_date,download_folder):
     for key,value in table_dict.items():
-        print(key,value)
         retrieve_table(client,project_id,key, dataset_id, value,ingestion_date,download_folder)
 
  
@@ -68,6 +103,7 @@ def add_data_to_raw_table(gauth_cred, cred_json, client_config_file, folder_id, 
         gauth.SaveCredentialsFile(gauth_cred)
 
     drive = GoogleDrive(gauth)
+    
 
     # Search for the file with the specified name in the specified folder
     query = "title='%s' and '%s' in parents and trashed=false" % (filename, folder_id)
@@ -91,12 +127,14 @@ def add_data_to_raw_table(gauth_cred, cred_json, client_config_file, folder_id, 
     file.GetContentFile(local_file_name)
     df = pd.read_json(file['title'])
 
-    df['Staging_Raw_ID'] = [uuid.uuid4() for _ in range(len(df.index))]
-    df['Staging_Raw_ID'] = df['Staging_Raw_ID'].astype(str)
+    df['uuid'] = [uuid.uuid4() for _ in range(len(df.index))]
+    df['uuid'] = df['uuid'].astype(str)
 
+    current_date = datetime.now()
+    df['ingestion_date'] = current_date.strftime('%Y-%m-%d')
     # upload the DataFrame to the new BigQuery table
     credentials = service_account.Credentials.from_service_account_file(cred_json)
-    pandas_gbq.to_gbq(df, f'{project_id}.{dataset_id}.{table_name}', project_id=project_id, credentials=credentials)
+    pandas_gbq.to_gbq(df, f'{project_id}.{dataset_id}.{table_name}', project_id=project_id, credentials=credentials,if_exists='append')
     os.remove(file['title'])
 
 
@@ -149,16 +187,6 @@ def update_staging_raw(client, project_id, dataset_id, dst_table_name, src_table
     client.delete_table(dataset_ref)
 
 
-def update_dim_stock(client, project_id, dataset_id, dst_table_name, src_table_name):
-    update_table_script = load_query("update_table_{}".format(dst_table_name)).format(
-        project_id=project_id,
-        dataset_id=dataset_id,
-        dst_table_name=dst_table_name,
-        src_table_name=src_table_name
-    )
-    client.query(update_table_script)
-
-
 def update_dim_date(client, project_id, dataset_id, date_table_name):
     update_table_script = load_query("update_table_{}".format(date_table_name)).format(
         project_id=project_id,
@@ -166,7 +194,6 @@ def update_dim_date(client, project_id, dataset_id, date_table_name):
         date_table_name=date_table_name
     )
     client.query(update_table_script)
-
 
 def update_fact_table(client, project_id, dataset_id, dst_table_name, src_table_name):
     update_table_script = load_query("update_table_{}".format(dst_table_name)).format(
@@ -176,6 +203,15 @@ def update_fact_table(client, project_id, dataset_id, dst_table_name, src_table_
         src_table_name=src_table_name
     )
     client.query(update_table_script)
+
+def update_dim_table(client, project_id, dataset_id, dst_table_name, src_table_name):
+    update_table_script = load_query("update_table_{}".format(dst_table_name)).format(
+        project_id=project_id,
+        dataset_id=dataset_id,
+        dst_table_name=dst_table_name,
+        src_table_name=src_table_name
+    )
+    client.query(update_table_script)    
 
 def check_table(client, project_id, dataset_id, table_name):
     check_table_script = load_query("check_table.sql").format(
